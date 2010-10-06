a389c49b3c7dc81e8aab8fe73826fc7d3a7e2f8f
Steven Van Bael <steven.v.bael@gmail.com>
2010-10-06 22:41:35 +0200

    added topbar

--- quickcopy/src/be/vbsteven/quickcopy/Global.java
+++ quickcopy/src/be/vbsteven/quickcopy/Global.java
@@ -30,7 +30,7 @@ public class Global {
 
 
 	public static boolean isLightTheme(Context context) {
-		return getPrefs(context).getString("integration.theme", "Dark theme").equals("Light theme");
+		return getPrefs(context).getString("integration.theme", "Light theme").equals("Light theme");
 	}
 
 
