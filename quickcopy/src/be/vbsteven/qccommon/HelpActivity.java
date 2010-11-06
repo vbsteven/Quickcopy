4d630cf0f0d19191dfbe6fb519e0e3b5842f5db9
Steven Van Bael <steven.v.bael@gmail.com>
2010-11-06 12:39:07 +0100

    added button to buy full version in help

--- quickcopy/src/be/vbsteven/qccommon/HelpActivity.java
+++ quickcopy/src/be/vbsteven/qccommon/HelpActivity.java
@@ -1,6 +1,11 @@
 package be.vbsteven.qccommon;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import be.vbsteven.quickcopy.R;
 
 
@@ -11,6 +16,15 @@ public class HelpActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.helpactivity);
-		
+		Button but = (Button)findViewById(R.id.button_buy_full);
+		but.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent goToMarket = null;
+				goToMarket = new Intent(Intent.ACTION_VIEW,Uri.parse("market://details?id=be.vbsteven.quickcopyfull"));
+				startActivity(goToMarket);
+			}
+		});
 	}
 }
