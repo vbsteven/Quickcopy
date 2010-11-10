ce077bb2cf048bd5d139011583c1e4113e45f491
Steven Van Bael <steven.v.bael@gmail.com>
2010-11-10 02:04:21 +0100

    added share button

--- quickcopy/src/be/vbsteven/qccommon/HelpActivity.java
+++ quickcopy/src/be/vbsteven/qccommon/HelpActivity.java
@@ -1,8 +1,11 @@
 package be.vbsteven.qccommon;
-import be.vbsteven.quickcopyfull.R;
-import be.vbsteven.quickcopyfull.R.layout;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import be.vbsteven.quickcopyfull.R;
 
 
 public class HelpActivity extends Activity {
@@ -13,5 +16,18 @@ public class HelpActivity extends Activity {
 		
 		setContentView(R.layout.helpactivity);
 		
+		
+		
+		Button shareButton = (Button)findViewById(R.id.button_share);
+		shareButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent share = new Intent(Intent.ACTION_SEND);
+				share.setType("text/plain");
+				share.putExtra(Intent.EXTRA_TEXT, "I am using Quickcopy. An #Android app that allows me to easily save and reuse text snippets. http://bit.ly/bWoKjW");
+				startActivity(share);
+			}
+		});
 	}
 }
